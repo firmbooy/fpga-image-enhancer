b6bac40729790b4b7652c6e23a646cd1acf51ca1
Vaibhav Balloli <26684166+vbbphc@users.noreply.github.com>
2018-11-09 21:59:13 +0530

    Adds: high boost filtering

---
+++ high_boost.m
@@ -0,0 +1,18 @@
+clc;
+clear;
+
+w_hp = [-1 -1 -1; -1 -8 -1;-1 -1 -1];
+% w_hp = [0 -1 0; -1 -4 -1; 0 -1 0];
+
+c = 2;
+w_ap = eye(3, 3);
+
+w_hb = w_ap + c * w_hp;
+
+image = imread('moon.jpg');
+
+hbf_output = uint8(filter2(w_hp, image, 'same'));
+
+hbf_sharpened_image = imsubtract(image, hbf_output);
+
+imshowpair(image, hbf_sharpened_image, 'montage')
